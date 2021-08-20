46fb669c0d3c1ee276b1da8ea6c92c703c3f4b16
NightMareStories <73705208+NightMareStories@users.noreply.github.com>
2021-08-20 22:38:49 +0300

    Added fonts, a folder for pictures and a new component App.

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import './App.scss';
+/* <picture>
+                    <source srcSet="img/about-text-img.webp" type="image/webp" />
+                    <img src="img/about-text-img.png" alt="" />
+                </picture> */
+
+function App() {
+    return (
+        <>
+        </>
+    );
+}
+
+export default App;
