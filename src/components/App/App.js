439b58b4874b13841bf35910447bc2107db598c3
NightMareStories <73705208+NightMareStories@users.noreply.github.com>
2021-08-21 23:34:31 +0300

    Added a header, made a markup on the BEM

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,7 @@
+import Header from '../Header/Header';
+
 import './App.scss';
+
 /* <picture>
                     <source srcSet="img/about-text-img.webp" type="image/webp" />
                     <img src="img/about-text-img.png" alt="" />
@@ -7,6 +10,7 @@ import './App.scss';
 function App() {
     return (
         <>
+            <Header />
         </>
     );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,40 @@
+import './Header.scss';
+
+function Header() {
+    return (
+        <>
+            <header className="header">
+                <div className="header__top top-header">
+                    <div className="top-header__content _container">
+                        <div className="top-header__column header-info">
+                            <div className="header-info__logo">
+                                <picture>
+                                    <source srcSet="img/logo-img.webp" type="image/webp" className="header-info__logo_img" />
+                                    <img src="img/logo-img.png" alt="" className="header-info__logo_img" />
+                                </picture>
+                            </div>
+                            <div className="header-info__text">
+                                <div className="header-info__text_username">
+                                    Poteryaev<br /> Aleksandr
+                                </div>
+                                <div className="header-info__text_userprof">
+                                    Front-End Developer
+                                </div>
+                            </div>
+                        </div>
+                        <div className="top-header__column header-nav">
+
+                        </div>
+                    </div>
+                </div>
+                <div className="header__bootom">
+                    <div className="bottom-header__content _container">
+
+                    </div>
+                </div>
+            </header>
+        </>
+    );
+}
+
+export default Header;
