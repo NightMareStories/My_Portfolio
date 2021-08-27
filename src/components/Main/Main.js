4e38585f03333a0d236e9699582471d9b8bc6cdc
NightMareStories <sp202037@yandex.ru>
2021-08-27 19:38:12 +0300

    Added portfolio block and navigation to it.

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -38,8 +38,8 @@ function Main() {
                             </a>
                         </div>
                         <div className="info-block__buttons info-buttons">
-                            <a href="" className="info-buttons__btn">HIRE ME</a>
-                            <a href="" className="info-buttons__btn">SEE MY RESUME</a>
+                            <a href="" className="info-buttons__button _btn">HIRE ME</a>
+                            <a href="" className="info-buttons__button _btn">SEE MY RESUME</a>
                         </div>
                     </div>
                     <div className="intro-block__picture">
@@ -47,6 +47,123 @@ function Main() {
                             <source srcSet="img/background-intro-img.webp" type="image/webp" />
                             <img src="img/background-intro-img.png" alt="" />
                         </picture>
+                    </div>
+                </div>
+            </section>
+
+            <section className="page__portfolio portfolio-block _container">
+                <div className="portfolio-block__content">
+                    <div className="portfolio-block__nav portfolio-nav">
+                        <a href="" className="portfolio-nav__filter">All</a>
+                        <a href="" className="portfolio-nav__filter">App</a>
+                        <a href="" className="portfolio-nav__filter">Website</a>
+                    </div>
+                    <div className="portfolio-block__portfolio portfolio-content">
+                        <div className="portfolio-content__column">
+                            <div className="portfolio-content__item portfolio-item">
+                                <a href="" className="portfolio-item__link">
+                                    <picture>
+                                        <source srcSet="img/sport-shop-img.webp" type="image/webp" />
+                                        <img src="img/sport-shop-img.png" alt="" />
+                                    </picture>
+                                </a>
+                                <div className="portfolio-item__text portfolio-text">
+                                    <div className="portfolio-text__category">Category</div>
+                                    <div className="portfolio-text__title">
+                                        Title
+                                        <time className="portfolio-text__date" dateTime="">Date</time>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="portfolio-content__column">
+                            <div className="portfolio-content__item portfolio-item">
+                                <a href="" className="portfolio-item__link">
+                                    <picture>
+                                        <source srcSet="img/sport-shop-img.webp" type="image/webp" />
+                                        <img src="img/sport-shop-img.png" alt="" />
+                                    </picture>
+                                </a>
+                                <div className="portfolio-item__text portfolio-text">
+                                    <div className="portfolio-text__category">Category</div>
+                                    <div className="portfolio-text__title">
+                                        Title
+                                        <time className="portfolio-text__date" dateTime="">Date</time>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="portfolio-content__column">
+                            <div className="portfolio-content__item portfolio-item">
+                                <a href="" className="portfolio-item__link">
+                                    <picture>
+                                        <source srcSet="img/sport-shop-img.webp" type="image/webp" />
+                                        <img src="img/sport-shop-img.png" alt="" />
+                                    </picture>
+                                </a>
+                                <div className="portfolio-item__text portfolio-text">
+                                    <div className="portfolio-text__category">Category</div>
+                                    <div className="portfolio-text__title">
+                                        Title
+                                        <time className="portfolio-text__date" dateTime="">Date</time>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="portfolio-content__column">
+                            <div className="portfolio-content__item portfolio-item">
+                                <a href="" className="portfolio-item__link">
+                                    <picture>
+                                        <source srcSet="img/sport-shop-img.webp" type="image/webp" />
+                                        <img src="img/sport-shop-img.png" alt="" />
+                                    </picture>
+                                </a>
+                                <div className="portfolio-item__text portfolio-text">
+                                    <div className="portfolio-text__category">Category</div>
+                                    <div className="portfolio-text__title">
+                                        Title
+                                        <time className="portfolio-text__date" dateTime="">Date</time>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="portfolio-content__column">
+                            <div className="portfolio-content__item portfolio-item">
+                                <a href="" className="portfolio-item__link">
+                                    <picture>
+                                        <source srcSet="img/sport-shop-img.webp" type="image/webp" />
+                                        <img src="img/sport-shop-img.png" alt="" />
+                                    </picture>
+                                </a>
+                                <div className="portfolio-item__text portfolio-text">
+                                    <div className="portfolio-text__category">Category</div>
+                                    <div className="portfolio-text__title">
+                                        Title
+                                        <time className="portfolio-text__date" dateTime="">Date</time>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="portfolio-content__column">
+                            <div className="portfolio-content__item portfolio-item">
+                                <a href="" className="portfolio-item__link">
+                                    <picture>
+                                        <source srcSet="img/sport-shop-img.webp" type="image/webp" />
+                                        <img src="img/sport-shop-img.png" alt="" />
+                                    </picture>
+                                </a>
+                                <div className="portfolio-item__text portfolio-text">
+                                    <div className="portfolio-text__category">Category</div>
+                                    <div className="portfolio-text__title">
+                                        Title
+                                        <time className="portfolio-text__date" dateTime="">Date</time>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="portfolio-block__button portfolio-button">
+                        <button type="button" className="portfolio-button__btn _btn">load more work</button>
                     </div>
                 </div>
             </section>
